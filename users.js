22c24928c20e06305b11658977ee76ee7299f394
Rose, Ian Christian <ianrose@iu.edu>
2026-04-01 22:19:45 -0400

    local version so testing is less annoying

--- users.js
+++ users.js
@@ -1,5 +1,5 @@
-//const host = "http://localhost:8080"
-const host = "https://ordersystem-theextracrispy.onrender.com"
+const host = "http://localhost:8080"
+//const host = "https://ordersystem-theextracrispy.onrender.com"
 
 const loginDropdown = document.getElementById("login");
 const newUserForm = document.getElementById('newUser');
