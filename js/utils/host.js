326c3aa3c262e1d9d40e76f9203f15cec1edc534
Rose, Ian Christian <ianrose@iu.edu>
2026-04-07 23:23:59 -0400

    Update host.js

--- js/utils/host.js
+++ js/utils/host.js
@@ -1,3 +1,3 @@
 //so i can change it in one place
-export const host = "http://localhost:8080";
-//export const host = "https://ordersystem-theextracrispy.onrender.com";
+//export const host = "http://localhost:8080";
+export const host = "https://ordersystemweek2.onrender.com";
